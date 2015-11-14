a9015fc007b10c6c641bf68f5a93802d39d0441a
Kai Wu <quietcool@163.com>
2015-11-14 22:58:35 +0800

    "1st step in refractoring functional_tests.py by adding func check_for_row_in_list_table."

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -28,38 +33,27 @@ class NewVisitorTest(unittest.TestCase):
             'Enter a to-do item'
         )
 
-        # She types "Buy peacock feathers" into a text box (Edith's hobby
-        # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
-
-        # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
+        self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
         # import time
         # time.sleep(10)
+
+        # 测试是否保存清单
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        # self.assertTrue(
-        # any(row.text == '1: Buy peacock feathers' for row in rows),
-        # "New to-do item did not appear in table--its text was:\n%s" %(table.text,)
-        # )
+
         self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2:Use peacock feathers to make a fly', [row.text for row in rows] )
+        self.assertIn('2:Use peacock feathers to make a fly', [row.text for row in rows])
 
-        # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly" (Edith is very
-        # methodical)
         self.fail('Finish the test!')
-
-        # The page updates again, and now shows both items on her list
-
-        # Edith wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
-
-        # She visits that URL - her to-do list is still there.
-
-        # Satisfied, she goes back to sleep
 
 
 if __name__ == '__main__':
