eb856b345da071eb7cd1b1a3c34b5d6d8267b829
Kai Wu <quietcool@163.com>
2015-11-15 14:55:05 +0800

    "make imigrations by changing models.py linking to django models - textfield"

--- lists/views.py
+++ lists/views.py
@@ -8,5 +8,4 @@ def home_page(request):
     # return HttpResponse('<html><title>To-Do lists</title><html>')
     # if request.method == 'POST':
     # return HttpResponse(request.POST['item_text'])
-    return render(request, 'home.html', {'new_item_text':
-                                             request.POST.get('item_text', ''), })
+    return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', ''), })
